609d10d6c47d336f5ab34c26fbed3d00ff220ffe
rocojeielc30 <14806@holbertonstudents.com>
2026-09-22 12:06:19 +1000

    made codestyle changes

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -2,16 +2,14 @@
 
 import math
 
-lang = "Language: Python";
-ver = "Version: 3";
-pi = math.pi;
-bool = 1 > 0;
-txt_pi = f"Pi approx: {pi:.2f}";
-comp = f"Computation valid: {bool}";
+lang = "Language: Python"
+ver = "Version: 3"
+pi = math.pi
+bool = 1 > 0
+txt_pi = f"Pi approx: {pi:.2f}"
+comp = f"Computation valid: {bool}"
 
-print(lang);
-print(ver);
-print(txt_pi);
-print(comp);
-
-
+print(lang)
+print(ver)
+print(txt_pi)
+print(comp)
