9e0508d38c8403c543b75910a77a354b36c11553
rocojeielc30 <14806@holbertonstudents.com>
2026-09-22 11:53:08 +1000

    first fix - for stdout length

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -7,7 +7,7 @@ ver = "Version: 3";
 pi = math.pi;
 bool = 1 > 0;
 txt_pi = f"Pi approx: {pi:.2f}";
-comp = f"Computation valid: {bool} ";
+comp = f"Computation valid: {bool}";
 
 print(lang);
 print(ver);
