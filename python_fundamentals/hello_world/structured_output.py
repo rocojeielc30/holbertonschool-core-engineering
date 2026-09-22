67973c2298e70d2ad256eed01533b6c9d5e2ced0
rocojeielc30 <14806@holbertonstudents.com>
2026-09-22 11:51:48 +1000

    create first script doe

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 
-print("Hello test");
+import math
+
+lang = "Language: Python";
+ver = "Version: 3";
+pi = math.pi;
+bool = 1 > 0;
+txt_pi = f"Pi approx: {pi:.2f}";
+comp = f"Computation valid: {bool} ";
+
+print(lang);
+print(ver);
+print(txt_pi);
+print(comp);
+
+
