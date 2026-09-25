c26fbd62c09460a4e9ecda1799a815d6fc4776e5
rocojeielc30 <14806@holbertonstudents.com>
2026-09-25 15:50:56 +1000

    fix for checker

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python3
-letters_to_print = ""
-# for every character in the string that I gave
-for char in "abcdefghijklmnopqrstuvwxyz":
-    # if the character is not q and is not e
-    if char != "q" and char != "e":
-        # append the letter into letters to print
-        letters_to_print += char
-
-print(letters_to_print)
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{}{}".format(chr(i), "\n" if i == 122 else ""), end="")
