08ecdb2f35b8245c0b51e82cf0d7f76555c37448
rocojeielc30 <14806@holbertonstudents.com>
2026-09-25 15:53:47 +1000

    change code so that it only has one loop

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 for i in range(97, 123):
+    # ASCII value for lowercase range a-z
     if i != 101 and i != 113:
+        # ASCII value for q and e
         print("{}{}".format(chr(i), "\n" if i == 122 else ""), end="")
+        # 1st {} gets chr(i) - 2nd {} gets the \n or empty string
