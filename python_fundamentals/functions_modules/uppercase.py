1ad3357a28edf6b5cea961d871565f91fca1e733
rocojeielc30 <14806@holbertonstudents.com>
2026-09-26 17:16:23 +1000

    trying to fix print function error

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 def uppercase(str):
-    string_to_print = ""
+    string_to_ = ""
     for letter in str:
         char_to_ascii = ord(letter)
         if 97 <= char_to_ascii <= 122:  # if the character is a lowercase
             char_to_ascii = char_to_ascii - 32  # convert to uppercase
             letter = chr(char_to_ascii)  # convert from ASCII back to letter
-            string_to_print += letter  # append
+            string_to_ += letter  # append
         else:
-            string_to_print += letter
-    print("{}".format(string_to_print))
+            string_to_ += letter
+    print("{}".format(string_to_))
